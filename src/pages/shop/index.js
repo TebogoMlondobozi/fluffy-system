ba3f7ec718e656bed4a9983f1d901446cf9dab40
ncedile <ncedile@mcmsolutions.co.uk>
2022-01-23 19:28:37 +0200

    shop page updates

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -17,35 +17,12 @@ export default function Shop() {
         <h1 className="pt-4">LAUNDRY SERVICES</h1>
       </div>
 
-      <div className="grid grid-cols-3 w-2/3 space-x-8">
-        <div className="text-left col-span-1">
-          <h2 className="mb-4">Categories</h2>
-          <ul className="w-1/2 text-left">
-            <li className="rounded-lg text-center hover:text-white hover:bg-blue-400 hover:cursor-pointer">
-              <NavLink to="/shop/all">All</NavLink>
-            </li>
-            <li className="rounded-lg text-center hover:text-white hover:bg-blue-400 hover:cursor-pointer">
-              <NavLink to="/shop/shirts">Shirts</NavLink>
-            </li>
-            <li className="rounded-lg text-center hover:text-white hover:bg-blue-400 hover:cursor-pointer">
-              <NavLink to="/shop/trousers">Trousers</NavLink>
-            </li>
-            <li className="rounded-lg text-center hover:text-white hover:bg-blue-400 hover:cursor-pointer">
-              <NavLink to="/shop/suits">Suits</NavLink>
-            </li>
-            <li className="rounded-lg text-center hover:text-white hover:bg-blue-400 hover:cursor-pointer">
-              <NavLink to="/shop/coats">Coats</NavLink>
-            </li>
-            <li className="rounded-lg text-center hover:text-white hover:bg-blue-400 hover:cursor-pointer">
-              <NavLink to="/shop/blankets">Blankets</NavLink>
-            </li>
-          </ul>
-        </div>
+      <div className="flex justify-center items-center">
         {!catalog ? (
           <p>Loading services...</p>
         ) : (
-          <div className="col-span-2">
-            <h1 className="mb-4">
+          <div className="flex flex-col space-x-10">
+            <h1 className="text-center mb-4">
               We ofer laundry services for the following variety of clothes
             </h1>
             <ul className="grid grid-cols-3">
