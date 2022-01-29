7c9720b73e482728c5cbcb39e9235320cfe648a6
ncedile <ncedile@mcmsolutions.co.uk>
2022-01-30 00:21:19 +0200

    adds skeleton loader

--- src/pages/shop/catalog/catalog-product.js
+++ src/pages/shop/catalog/catalog-product.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 import { useNavigate, generatePath } from "react-router-dom";
 import PropTypes from "prop-types";
 
@@ -8,6 +11,7 @@ import { addCartItem, removeCartItem } from "../../../features/cart/cartSlice";
 export default function CatalogProduct({ product }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [imageIsLoading, setImageIsLoading] = useState(false);
 
   return (
     <li key={product._id} className="flex flex-col space-y-4">
@@ -16,11 +20,25 @@ export default function CatalogProduct({ product }) {
         onClick={() => navigate(generatePath(`/product/:_id`, product))}
       >
         <div className="w-60">
-          <img src={product.img.dataUrl} alt="not available" />
+          {!imageIsLoading ? <Skeleton count={1} className="h-36" /> : null}
+          <img
+            style={imageIsLoading ? {} : { display: "none" }}
+            src={product.img.dataUrl}
+            alt="not available"
+            onLoad={() => setImageIsLoading(true)}
+          />
         </div>
         <div className="flex flex-col">
-          <span>{product.name}</span>
-          <span className="truncate">{product.description}</span>
+          {!imageIsLoading ? (
+            <div className="w-40">
+              <Skeleton count={2} className="h-5" />
+            </div>
+          ) : (
+            <>
+              <span>{product.name}</span>
+              <span className="truncate">{product.description}</span>
+            </>
+          )}
         </div>
       </div>
       <div className="flex items-center space-x-4">
