9cfbc0a9e7a2863533df07383cf699fea7d8b91d
ncedile <ncedile@mcmsolutions.co.uk>
2022-01-30 03:52:22 +0200

    small updates

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,24 +4,23 @@ import { PageLayout } from "../../components/structure";
 export default function Home() {
   return (
     <PageLayout>
-      <div className="flex items-center">
-        <div className="w-full flex justify-between">
-          <div>
-            <h1>
-              WELCOME TO <span className="text-blue-500">EAZIWASH</span>
-            </h1>
-            <p>WE OFFER FULL ON DEMAND LAUNDRY SERVICE</p>
+      <div className="p-20 w-full flex justify-between">
+        <div className="border-r-2 border-dotted pr-10">
+          <h1 className="text-lg font-bold">
+            WELCOME TO <span className="text-blue-500">EAZIWASH</span>
+          </h1>
+          <p className="text-lg font-bold">
+            WE OFFER FULL ON DEMAND DRY CLEANING &amp; LAUNDRY SERVICE
+          </p>
 
-            <div>
-              <ul>
-                <li>Home Pickup & Delivery</li>
-                <li>Timely Turnaround</li>
-                <li>Affordable Prices</li>
-                <li>Satisfaction Guaranteed</li>
-              </ul>
-            </div>
+          <div className="p-10">
+            <ul className="list-disc">
+              <li>Home Pickup &amp; Delivery</li>
+              <li>Timely Turnaround</li>
+              <li>Affordable Prices</li>
+              <li>Satisfaction Guaranteed</li>
+            </ul>
           </div>
-          <div>Login | account</div>
         </div>
       </div>
     </PageLayout>
